06b31b916ae0b54d9d9c580c7328c418ddc48fed
Robert Brandl <rbrandl@stevens.edu>
2024-04-29 20:31:10 -0400

    added redis caching for getAllPlaylists

--- backend/routes/playlists.js
+++ backend/routes/playlists.js
@@ -2,6 +2,9 @@ import { Router } from "express";
 const router = Router();
 import { playlistData } from "../data/index.js";
 import {searchData} from "../config/elasticSync.js" ;
+import { createClient } from 'redis';
+const client = createClient();
+client.connect().then(() => {});
 //route to get a single song route based on its id
 router.route("/playlist/:id").get(async (req, res) => {
     try{
@@ -12,11 +15,19 @@ router.route("/playlist/:id").get(async (req, res) => {
     }
   });
 router.route("/allplaylists").get(async (req, res) => {
-    try{
-        const data = await playlistData.getAllPlaylists();
-        return res.status(200).json(data);
-    }catch(e){
-        return res.status(500).json({error: e});
+    let exists = await client.exists(`allplaylists`);//this helps with the elasticsearch bc less syncing needed!
+    if (exists) {
+        let result = await client.get(`allplaylists`);
+        return res.status(200).json(JSON.parse(result));
+    }
+    else{
+        try{
+            const data = await playlistData.getAllPlaylists();
+            await client.SETEX(`allplaylists`, 3600, JSON.stringify(data));
+            return res.status(200).json(data);
+        }catch(e){
+            return res.status(500).json({error: e});
+        }
     }
 });
 router.route("/searchbyname").get(async (req, res) => {
