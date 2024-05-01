e1096d33430a4b8f5f2ec78974f664b95fe71290
rivaldo dsilva <rivaldodsilva8@gmail.com>
2024-05-01 03:19:12 -0400

    added add to playlist button on SingleSong, css to SingleSong, few changes to Login and Setup CSS Files

--- frontend/src/pages/SingleSong/SingleSong.jsx
+++ frontend/src/pages/SingleSong/SingleSong.jsx
@@ -2,10 +2,16 @@ import "./SingleSong.css";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom"; 
 import axios from "axios";
+export const PlaylistList = () => {
+  return (
+    <div className="existing-playlist"><ul className="playlist-list"><li><button className="single-playlist-add">Create New Playlist + </button></li><li><button className="single-playlist-add">Playlist 1 </button> </li><li><button className="single-playlist-add">Playlist 2 </button></li><li><button className="single-playlist-add">Playlist 3</button></li></ul></div>
+  )
+}
 export const SingleSong = () => {
   const [songData, setSongData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [errorMessage, setErrorMessage] = useState('');
+  const [visible,setVisible] = useState(true);
   const { id } = useParams(); 
 
   useEffect(() => {
@@ -41,18 +47,24 @@ export const SingleSong = () => {
   return <div>No song found with ID: {id}</div>;
 }
   return (<>
-  <div className="single-song">{songData.title && <h1>{songData.title}</h1>}</div> 
+  <div className="single-song">{songData.title && <h1>{songData.title}</h1>}
+  <button className="add-to-playlist" onClick={() => {setVisible(prev => !prev)}}>Add To Playlist</button>
+ 
   {songData.artist.name &&  <h2>By : {songData.artist.name}</h2>}
+ 
+
   {songData.artist.picture_medium && <img src={songData.artist.picture_medium} alt={songData.artist.picture_medium} className="artist-image"/>}
+ 
   {songData.release_date &&  <h2>Release Date: {songData.release_date}</h2>}
+  {visible && <PlaylistList/>}
+  
   {songData.album.cover_medium && <img src={songData.album.cover_medium} alt={songData.album.cover_medium} />}
   {songData.duration && <h3>Duration : {secondsToMinutes(songData.duration)}</h3>}
   {songData.explicit_lyrics ? <h3 className="explicit">Explicit</h3>: <h3>Non-Explicit</h3>}
   {songData.preview &&   <video controls={true}  className="preview"><source src={songData.preview} type="audio/mpeg"/></video>}
-  {songData.album && <h3>Album : {songData.album.title}</h3>}
+  {songData.album && <h2>Album : {songData.album.title}</h2>}
+  </div> 
   
- 
-
 
   </>
 
@@ -60,3 +72,5 @@ export const SingleSong = () => {
   
  
 };
+
+
