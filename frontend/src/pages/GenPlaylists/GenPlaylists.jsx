c53a453e3fce7b3eaf492807f0c7a3388695ec73
Robert Brandl <rbrandl@stevens.edu>
2024-05-05 20:19:32 -0400

    added save playlist to all playlists page and css changes

--- frontend/src/pages/GenPlaylists/GenPlaylists.jsx
+++ frontend/src/pages/GenPlaylists/GenPlaylists.jsx
@@ -9,13 +9,38 @@ export const GenPlaylists = () => {
   const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState('');
   const [error, setError] = useState('');
+  const [playlistStates, setPlaylistStates] = useState({});
   useEffect(() => {
     setError('');
+    let res = null;
+    async function getUser(){
+      let data = await axios.get('/api/users/account', {
+        params: {
+          email: currentUser.email
+        }
+      });
+      res = data.data
+    }
     async function fetchAllData() {
       setLoading(true);
       try{
         const {data} = await axios.get(`/api/playlists/allplaylists`);
         console.log(data)
+        if (data && data.length > 0) {
+          const updatedStates = {};
+          for (let x of data) {
+            if (res && res.savedPlaylists && res.savedPlaylists.includes(x._id.toString())) {
+              updatedStates[x._id] = true;
+            }
+            else if (res && res.savedPlaylists){
+              updatedStates[x._id] = false;
+            }
+          }
+          setPlaylistStates(prevStates => ({
+            ...prevStates,
+            ...updatedStates
+          }));
+        }
         setPlaylistData(data)
         setError("");
       }catch(e){
@@ -30,6 +55,21 @@ export const GenPlaylists = () => {
           name: searchTerm
       }});
       console.log(data)
+      if (data && data.length > 0) {
+        const updatedStates = {};
+        for (let x of data) {
+          if (res && res.savedPlaylists && res.savedPlaylists.includes(x._id.toString())) {
+            updatedStates[x._id] = true;
+          }
+          else if (res && res.savedPlaylists){
+            updatedStates[x._id] = false;
+          }
+        }
+        setPlaylistStates(prevStates => ({
+          ...prevStates,
+          ...updatedStates
+        }));
+      }
         let pdata = []
         if(data && data.length > 0){
           pdata = data.map((e) => e._source);
@@ -42,6 +82,7 @@ export const GenPlaylists = () => {
       }
       //setLoading(false);
     }
+    getUser();
     if (searchTerm){
       fetchData();
     }
@@ -61,6 +102,36 @@ export const GenPlaylists = () => {
     const year = date.getFullYear();
     return `${month}/${day}/${year}`;
   }
+  const handleSave = async (playlistId) => {
+    setLoading(true);
+    const isPlaylistSaved = playlistStates[playlistId];
+    console.log(playlistStates);
+    if (isPlaylistSaved === false){
+      try{
+      const response = await axios.patch('/api/users/save', {
+        email: currentUser.email,
+        saveId: playlistId
+      });
+      }catch(e){
+        setError(e.response.statusText || e.message)
+      }
+    }
+    else{
+      try{
+      const response = await axios.patch('/api/users/unsave', {
+        email: currentUser.email,
+        unsaveId: playlistId
+      });
+      }catch(e){
+        setError(e.response.statusText || e.message)
+      }
+    }
+    setPlaylistStates(prevStates => ({
+      ...prevStates,
+      [playlistId]: !isPlaylistSaved
+    }));
+    setLoading(false);
+  };
 
   if (loading){
     return <div>Loading...</div>
@@ -71,6 +142,7 @@ export const GenPlaylists = () => {
   return (
     <div className='card'>
       <h1>View and Search Through All Playlists</h1>
+      <br />
       <form
         method='POST'
         onSubmit={(e) => {
@@ -81,6 +153,7 @@ export const GenPlaylists = () => {
       >
         <label>
           <span>Search for a Playlist by Title: </span>
+          <br />
           <input
             autoComplete='off'
             type='text'
@@ -88,21 +161,28 @@ export const GenPlaylists = () => {
             value={searchTerm}
             onChange={handleChange}
             autoFocus
+            className="inpclass"
+            placeholder="Search for a playlist..."
           />
         </label>
       </form>
+      <br />
       {playlistData && playlistData.length > 0 ? 
       playlistData.map((playlist) => (
         <li key={playlist._id}>
           <Link
-            className="linker"
+          className="linker"
             to={`/playlist/${playlist._id}`}
           >
             <span>{playlist.title}</span>
             <span className="created-by">
               Created By: {playlist.userName}
             </span>
-
+            {currentUser && playlist.userName !== currentUser.displayName  && (
+                <button onClick={() => handleSave(playlist._id)} className="save-button2">
+                  {playlistStates[playlist._id] ? 'Unsave Playlist' : 'Save Playlist'}
+                </button>
+              )}
             <span className="genre">
               {formatDate(playlist.dateCreated)}
             </span>
